86bfd820929221ab3a052004ff547d45282b6439
Shakil <sakildevmern@gmail.com>
2026-02-13 23:44:12 +0600

    2/13/2026

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -9,22 +9,41 @@
 </head>
 
 <body>
-    <section class="min-h-screen flex items-center justify-center">
-        <div class="text-center">
-            <h1 class="text-4xl font-bold mb-4">Welcome to Projuktirpathshala admin dashboard</h1>
-            <p class="text-lg text-gray-700">Manage students and their information here.</p>
-            <!-- <a href="{{ route('admin.index') }}" class="mt-6 inline-block text-blue-500">Go to Admin Panel</a> -->
-            <div class="flex items-center justify-center">
-                <div class="bg-slate-100 p-4">
-                    <h2>Student Information</h2>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas perferendis consequuntur, velit dolorem reiciendis dignissimos perspiciatis ad commodi cupiditate placeat rerum recusandae officiis mollitia? Perspiciatis labore, nisi tenetur maxime recusandae animi ipsam quas. Culpa reprehenderit, explicabo a ducimus veniam aut, et officiis maxime velit in laudantium voluptas eos labore! Quae?</p>
+    <section>
+        <div class="xl:w-5xl mx-auto">
+            <h1 class="text-3xl font-bold text-center mt-10">Hello Admin!</h1>
+            <p class="text-center mt-5 text-xl max-w-4xl mx-auto">
+                Welcome to the admin dashboard of Projuktirpathshala! Here you can manage all aspects of the platform, including students, courses, and more. Use the navigation menu to access different sections of the dashboard and stay organized with your administrative tasks.
+            </p>
+            <div class="mt-10 flex justify-between border-t pt-10">
+                <div>
+                    <div class="flex gap-x-20">
+                        <h2 class="text-2xl font-semibold">Students List</h2>
+                        <button onclick="handleAddStudent" class="bg-blue-500 hover:bg-blue-700 text-white py-1 px-4 rounded inline-block cursor-pointer">Add Student</button>
+                    </div>
+                    <div class="mt-5">
+                        <table class="min-w-full bg-white border">
+                            <thead>
+                                <tr>
+                                    <th class="py-2 px-4 border-b">ID</th>
+                                    <th class="py-2 px-4 border-b">Name</th>
+                                    <th class="py-2 px-4 border-b">Email</th>
+                                    <th class="py-2 px-4 border-b">Actions</th>
+                                </tr>
+                            </thead>
+
+                        </table>
+
+                    </div>
+
                 </div>
-                <div class="bg-slate-200 p-4">
-                    <h2>Student Certificate Management</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ipsam voluptatum minus aliquam, incidunt maiores. Quo doloremque, qui incidunt provident tempora atque debitis labore assumenda sint dolorem dolores ratione velit mollitia necessitatibus quam sit vitae deleniti omnis ipsam repudiandae excepturi corrupti eum architecto suscipit? Id obcaecati eligendi magnam fuga dolor?</p>
+                <div>
+                    <div class="flex gap-x-20">
+                        <h2 class="text-2xl font-semibold">Student Certificates</h2>
+                        <button onclick="handleAddCertificate" class="bg-blue-500 hover:bg-blue-700 text-white py-1 px-4 rounded inline-block cursor-pointer">Add Certificate</button>
+                    </div>
                 </div>
             </div>
-        </div>
     </section>
 </body>
 
